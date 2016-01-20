aa15684e1c3ae782d8f641e82c1168478cbc1749
Kyle Kelley <rgbkrk@gmail.com>
2016-01-20 11:11:16 -0600

    Lay out sample package.

---
+++ index.js
@@ -0,0 +1,9 @@
+var jupyterPaths = require('jupyter-paths');
+
+function launch(kernelName) {
+  // Search in each directory of jupyterPaths.paths.kernelSpecs
+}
+
+module.exports = {
+  launch,
+};
